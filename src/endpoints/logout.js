a834043c4d29b806e1a0056030544d7172717cb8
AVVS <v@aminev.me>
2015-12-23 11:35:39 +0300

    refactor: babel 6, peerDependencies

--- src/endpoints/logout.js
+++ src/endpoints/logout.js
@@ -2,9 +2,29 @@ const config = require('../config.js');
 const { getRoute, getTimeout, getAudience } = config;
 const ROUTE_NAME = 'logout';
 
+/**
+ * @api {post} /logout Invalidates JWT token
+ * @apiVersion 1.0.0
+ * @apiName Logout
+ * @apiGroup Users
+ * @apiPermission users
+ *
+ * @apiDescription Invalidates JWT token of an authenticated user, effectively logging the user out
+ *
+ * @apiHeader (Authorization) {String} Authorization JWT :accessToken
+ * @apiHeaderExample Authorization-Example:
+ * 		"Authorization: JWT myreallyniceandvalidjsonwebtoken"
+ *
+ * @apiExample {curl} Example usage:
+ *     curl -i -X POST -H 'Accept-Version: *' -H 'Accept: application/vnd.api+json' \
+ *       "https://api-users.sandbox.matic.ninja/api/users/logout"
+ *
+ * @apiSuccessExample {json} Success:
+ * 		HTTP/1.1 204 No Content
+ */
 exports.post = {
   path: '/logout',
-  middleware: [ 'auth' ],
+  middleware: ['auth'],
   handlers: {
     '1.0.0': function logout(req, res, next) {
       const message = {
